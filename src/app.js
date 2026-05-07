2a75df99501ad7bfcb4b5257f379c5b8d72d5ca2
anshuman-borah <anshuman15b@gmail.com>
2026-05-07 13:40:05 +0530

    local app.js changes

--- src/app.js
+++ src/app.js
@@ -1,20 +1,22 @@
 import express from 'express';
 import cors from 'cors';
 import authRouter from './routes/auth.route.js';
-import projectRouter from './routes/project.route.js';
+import projectRouter from './routes/project.route.js'; 
+import similarityRouter from './routes/similarity.route.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth",authRouter);
-app.use("/api/projects", projectRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/projects", projectRouter); 
+app.use("/api/similarity", similarityRouter);
+
 app.get("/health", (req, res) => {
     res.status(200).json({ status: "ok" });
-})
+});
 
-app.use(errorMiddleware)
-
+app.use(errorMiddleware);
 
 export default app;
